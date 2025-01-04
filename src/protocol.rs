39bcc541137f1b505e96855a2df1fa9b91597220
Jakub Zdanowicz-Zasidko <me@zzjakub.com>
2025-01-04 20:10:33 +0100

    fix(reliability): handle message encoding properly
    Acknowledge header conversion as fallible operationand allow caller
    to take care of the outcome.
    
    Introduce thiserror crate to reduce boilerplate.

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,7 +1,14 @@
 use bigdecimal::BigDecimal;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 pub mod event;
+
+#[derive(Error, Debug)]
+pub enum ConversionError {
+    #[error("Failed to convert message header to json: {0}")]
+    HeaderSerialization(#[from] serde_json::Error),
+}
 
 #[derive(Debug)]
 pub struct Message {
@@ -11,11 +18,11 @@ pub struct Message {
 }
 
 impl Message {
-    pub fn into_bytes(&self) -> Vec<u8> {
+    pub fn try_into_bytes(&self) -> Result<Vec<u8>, ConversionError> {
         let mut output = Vec::new();
 
         // Convert header to JSONL, and push it to the buffer
-        let header_json = serde_json::to_string(&self.header).expect("Header serialization failed");
+        let header_json = serde_json::to_string(&self.header)?;
         output.extend_from_slice(header_json.as_bytes());
         output.push(b'\n');
 
@@ -29,7 +36,7 @@ impl Message {
             output.extend_from_slice(&payload.0);
         }
 
-        output
+        Ok(output)
     }
 }
 
